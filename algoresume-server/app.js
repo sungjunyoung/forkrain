ea7d6a39dc001f4083a96d8508826e1805330aa3
Nildy <faithdy@gmail.com>
2017-09-16 02:16:04 +0900

    app.js 라우터에 urls, apis 패스 등록하였습니다.

--- algoresume-server/app.js
+++ algoresume-server/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var urls = require('./routes/urls');
+var apis = require('./routes/apis');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/urls', urls);
+app.use('/apis', apis);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
