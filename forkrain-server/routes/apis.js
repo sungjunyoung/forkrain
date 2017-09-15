d8d57335f4d0acb4013f826b7f507b2ec94aab69
sungjunyoung <wnsdud1861@gmail.com>
2017-09-16 07:57:01 +0900

    로그인 기능 구현을위한 사전준비

--- forkrain-server/routes/apis.js
+++ forkrain-server/routes/apis.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-console.log("create apis.js");
+
+router.get('/logout', function (req, res, next) {
+    req.session.destroy();  // 세션 삭제
+    // res.clearCookie(); // 세션 쿠키 삭제
+});
 
 module.exports = router;
--- forkrain-server/routes/login.js
+++ forkrain-server/routes/login.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+var passport = require('passport');
+router.use(require('cookie-parser')());
+router.use(require('body-parser').urlencoded({extended: true}));
+router.use(require('express-session')({secret: 'keyboard cat', resave: true, saveUninitialized: true}));
+
+router.use(passport.initialize());
+router.use(passport.session());
+var methodOverride = require('method-override');
+var GitHubStrategy = require('passport-github2').Strategy;
+
+
+router.get('/', function (req, res, next) {
     res.render('login');
 });
+
 
 module.exports = router;
