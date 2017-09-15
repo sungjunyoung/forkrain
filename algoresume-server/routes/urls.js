93b4bf708084def61a262f6c4c2f186e29caf8b4
Nildy <faithdy@gmail.com>
2017-09-16 02:43:58 +0900

    url.ejs, urls.ejs 데모 페이지 작성하였습니다. 기존 하위패스 파라미터는 url.ejs에 렌더링 하였습니다.

--- algoresume-server/routes/urls.js
+++ algoresume-server/routes/urls.js
@@ -4,12 +4,14 @@ var router = express.Router();
 
 /* GET urls listing. */
 router.get('/', function(req, res, next) {
-    res.send(req.params.name);
+    //res.send(req.params.name);
+    res.render('urls');
 });
 
-router.get('/:idx', function (req, res, next) {
-  var idx = req.params.idx;
-  res.send(idx);  
+
+router.get('/:url_id', function (req, res, next) {
+  var url_id = req.params.url_id;
+  res.render('url', { url_id : url_id});
 })
 
 module.exports = router;
