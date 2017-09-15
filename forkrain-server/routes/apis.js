a3999c08b58b0a5d9d1edfccb4698de12a233a83
Nildy <faithdy@gmail.com>
2017-09-16 08:17:05 +0900

    핀 클릭시 ON되는 경우는 insert로, OFF되는 경우는 delete로 정의하였고 db 삭제 잘됩니다.

--- forkrain-server/routes/apis.js
+++ forkrain-server/routes/apis.js
@@ -5,16 +5,30 @@ var Pin = require("./model/pin.js");
 
 console.log("create apis.js");
 
-router.post('/click', function (req, res, next) {
+router.post('/insert', function (req, res, next) {
+  /*TEST CODE*/
   var pin = new Pin({
     user_id : "API TEST",
     idx : 200
   });
+  /***********/
 
   pin.save((err, doc)=> {
     if(err) console.log(err);
     else console.log("save DB");
   });
-});
+})
+
+router.delete('/delete', function (req, res, next) {
+  /*TEST data*/
+  user_id = "API TEST";
+  idx = 200;
+  /***********/
+
+  Pin.deleteOne({user_id:user_id}, (err, doc)=> {
+    if(err) console.log(err);
+    else console.log("remove DB");
+  });
+})
 
 module.exports = router;
