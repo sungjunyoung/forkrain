f3d91ff21875eaca4cc8f06833c6eb1515a02705
Nildy <faithdy@gmail.com>
2017-09-16 06:21:17 +0900

    model 폴더 내에 pins 스키마 구현 레퍼런스 위해 ref-db.js 첨부합니다.

---
+++ forkrain-server/routes/ref-db.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var Pin = require("./model/pin.js");
+
+var pin = new Pin({
+  user_id : "Doyeon",
+  idx : 1
+});
+
+router.get('/', function(req, res, next) {
+  pin.save((err, doc)=> {
+    if(err) console.log(err);
+    else res.send("success");
+  });
+});
+
+module.exports = router;
