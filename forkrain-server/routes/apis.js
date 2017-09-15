7f43c257ac7b460c0d6bb814778147626f20b3b5
Nildy <faithdy@gmail.com>
2017-09-16 07:38:26 +0900

    click에 대한 api 구현하였습니다. 테스트를 위해 ref-page를 만들었습니다.

--- forkrain-server/routes/apis.js
+++ forkrain-server/routes/apis.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var Pin = require("./model/pin.js");
 
 console.log("create apis.js");
+
+router.post('/click', function (req, res, next) {
+  var pin = new Pin({
+    user_id : "API TEST",
+    idx : 200
+  });
+
+  pin.save((err, doc)=> {
+    if(err) console.log(err);
+    else console.log("save DB");
+  });
+});
 
 module.exports = router;
