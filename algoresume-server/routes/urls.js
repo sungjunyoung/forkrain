7774167dbdee93f163b8dfb1349890fd8aeb7308
Nildy <faithdy@gmail.com>
2017-09-16 03:18:22 +0900

    app.js 라우터에 users 등록하였습니다. users.js 하위패스에 대해 렌더링하였습니다.

--- algoresume-server/routes/urls.js
+++ algoresume-server/routes/urls.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var app = express();
 var router = express.Router();
 
 /* GET urls listing. */
@@ -11,7 +10,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/:url_id', function (req, res, next) {
   var url_id = req.params.url_id;
-  res.render('url', { url_id : url_id});
+  res.render('url', { url_id : url_id });
 })
 
 module.exports = router;
