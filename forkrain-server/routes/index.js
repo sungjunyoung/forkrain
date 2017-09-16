bc124fb19eec1e61a618d6daef7965378ffdad05
Nildy <faithdy@gmail.com>
2017-09-16 13:07:07 +0900

    ejs 로 데이터 넘기고, ejs 주석 제거

--- forkrain-server/routes/index.js
+++ forkrain-server/routes/index.js
@@ -3,23 +3,24 @@ var router = express.Router();
 var fs = require('fs');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // var text = fs.readFileSync('please-edit-me.txt','utf8');
-  // var articleArray = text.split(/\n+/);
-  // var articles = [];
-  //
-  // articleArray.forEach(function(str) {
-  //   if(str) {
-  //     var data = str.split(/\s+/);
-  //     var tags = [];
-  //     i=3;
-  //     while(data[i]) {
-  //       tags.push(data[i]);
-  //       i++;
-  //     }
-  //     articles.push({'idx':data[0], 'user_id':data[1], 'url':data[2], 'tags':tags});
-  //   }
-  // });
-  res.render('index', {title: 'Express'});
+  var text = fs.readFileSync('please-edit-me.txt','utf8');
+  var articleArray = text.split(/\n+/);
+  var articles = [];
+
+  articleArray.forEach(function(str) {
+    if(str) {
+      var data = str.split(/\s+/);
+      var tags = [];
+      i=3;
+      while(data[i]) {
+        tags.push(data[i]);
+        i++;
+      }
+      articles.push({'idx':data[0], 'user_id':data[1], 'url':data[2], 'tags':tags});
+    }
+    res.render('index', {data: articles});
+
+  });
 
 
 });
