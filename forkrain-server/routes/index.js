46317fee922f66e07c6254687062cb2834baa21d
Nildy <faithdy@gmail.com>
2017-09-16 10:46:15 +0900

    edit-me 텍스트 파일에서 JSON으로 파싱 구현

--- forkrain-server/routes/index.js
+++ forkrain-server/routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    // console.log(req.session);
-    res.render('index', {title: 'Express'});
+  // var text = fs.readFileSync('please-edit-me.txt','utf8');
+  // var articleArray = text.split(/\n+/);
+  // var articles = [];
+  //
+  // articleArray.forEach(function(str) {
+  //   if(str) {
+  //     var data = str.split(/\s+/);
+  //     var tags = [];
+  //     i=3;
+  //     while(data[i]) {
+  //       tags.push(data[i]);
+  //       i++;
+  //     }
+  //     articles.push({'idx':data[0], 'user_id':data[1], 'url':data[2], 'tags':tags});
+  //   }
+  // });
+  res.render('index', {title: 'Express'});
+
+
 });
 
 module.exports = router;
