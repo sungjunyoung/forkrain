c186a4774f42b1ec8c23a1ef72389de6b75116ce
sungjunyoung <wnsdud1861@gmail.com>
2017-09-17 06:08:13 +0900

    배포 테스트

--- forkrain-server/routes/auth.js
+++ forkrain-server/routes/auth.js
@@ -23,7 +23,9 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
         clientID: GITHUB_CLIENT_ID,
         clientSecret: GITHUB_CLIENT_SECRET,
-        callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+        // callbackURL: "http://127.0.0.1:3000/auth/github/callback",
+        callbackURL: "http://54.156.199.32:3000/auth/github/callback"
+
     },
     function (accessToken, refreshToken, profile, done) {
         // asynchronous verification, for effect...
